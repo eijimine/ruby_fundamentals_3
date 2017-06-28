a9683f81ecafeafec3c873a019e7b95b4780fd11
eiji <eijimine@gmail.com>
2017-06-28 14:09:29 -0400

    Exercise 0

---
+++ exercises.rb
@@ -0,0 +1,17 @@
+fav_colours = ['Blue', 'Black', 'Grey', 'Purple', 'Red']
+age = [35, 37, 34, 22, 23]
+coin = ['Heads', 'Tails', 'Tails', 'Heads', 'Heads']
+artists = ['Buckethead', 'Metallica', 'Marty Friedman']
+fav_colourss = [:Blue, :Black, :Grey, :Purple, :Red]
+
+words = {'Array' => 'Arrays are ordered, integer-indexed collections of any object.', 'Hash' => 'A Hash is a collection of unique keys and their values.', 'Pop' => 'To remove the last item from an array.'}
+movies = {'Fifth Element' => '1997', 'Whiplash' => '2015', 'The Professional' => '1994'}
+cities = {:Tokyo => '13 Million', :Toronto => '6.418 Million', :Montreal => '4.099 Million'}
+family = {:Hisa => 37, :Angela => 34, :Ronnie => 36, :Pat => 34}
+
+
+puts coin
+puts fav_colours[0]
+puts age.sort
+age << 0
+puts movies['Whiplash']
