ec08330355653c904b70a6ae2b3216319fbfe1d6
eiji <eijimine@gmail.com>
2017-06-28 15:25:32 -0400

    Updated with ex 5

--- exercises.rb
+++ exercises.rb
@@ -9,27 +9,35 @@ movies = {'Fifth Element' => '1997', 'Whiplash' => '2015', 'The Professional' =>
 cities = {:Tokyo => '13 Million', :Toronto => '6.418 Million', :Montreal => '4.099 Million'}
 family = {:Hisa => 37, :Angela => 34, :Ronnie => 36, :Pat => 34}
 
-puts coin
-puts fav_colours[0]
-puts age.sort
-age << 0
-puts movies['Whiplash']
-puts fav_colours[-1]
-cities[:Hong_Kong] = '7.306 Million'
-coin.reverse!
-puts "The population of Tokyo is #{cities[:Tokyo]}."
-
-artists.each do |artist|
-  puts "I think #{artist} is awesome!"
-end
-
-puts artists[0..1]
-
-
-# movies.each do |movie, year|
-#   puts "#{movie} came out in #{year}"
+# puts coin
+# puts fav_colours[0]
+# puts age.sort
+# age << 0
+# puts movies['Whiplash']
+# puts fav_colours[-1]
+# cities[:Hong_Kong] = '7.306 Million'
+# coin.reverse!
+# puts "The population of Tokyo is #{cities[:Tokyo]}."
+#
+# artists.each do |artist|
+#   puts "I think #{artist} is awesome!"
 # end
+#
+# puts artists[0..1]
+#
+# movies.each { |movie, year| puts "#{movie} came out in #{year}" }
+#
+# puts age.sort.reverse
+#
+# movies['Beauty and the Beast'] = ['1991', '2017']
+# puts movies
 
-movies.each { |movie, year| puts "#{movie} came out in #{year}" }
-
-puts age.sort.reverse
+# puts age.select { |num| num <= 30  }
+#
+# puts age.sort[-1]
+#
+# puts coin.count('Heads')
+#
+# artists.delete('Metallica')
+# cities['Toronto'] = '1'
+# puts cities
